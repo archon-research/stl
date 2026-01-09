d81c2a1e6b717aee77801c0d09350453d8ec414a
knaekbroed <simon.bojeoutzen@archonlabs.com>
2026-01-09 13:20:57 +0100

    Use make tools in devcontainer script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,12 +4,7 @@ set -e
 echo "==> Installing Go tools..."
 
 # Install Go development tools
-go install golang.org/x/tools/gopls@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install golang.org/x/vuln/cmd/govulncheck@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
+make tools
 
 echo "==> Installing project dependencies..."
 cd /workspaces/stl/stl-verify
